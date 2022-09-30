c97a5d88f4667aaab345bc56fa63e9eff2b52b44
MD. Rezaul Karim <rksazid@gmail.com>
2022-09-30 23:52:00 +0600

    autoload classic added for heroku

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Ndibd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload = :classic
 
     # Don't generate system test files.
     config.generators.system_tests = nil
